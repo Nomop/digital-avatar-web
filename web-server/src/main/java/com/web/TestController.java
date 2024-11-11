1e032078fdd147b4a935096948c422ef6743f197
Nomop <13098939400@163.com>
2024-11-11 14:34:40 +0800

    接入日志框架logback

--- web-server/src/main/java/com/web/TestController.java
+++ web-server/src/main/java/com/web/TestController.java
@@ -1,13 +1,17 @@
 package com.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
     @RequestMapping("/test")
     private String test(){
+        System.out.println("一行白鹭上青天");
+        log.info("log: 两个黄鹂鸣翠柳");
         return "===========QWQ==========";
     }
 }
